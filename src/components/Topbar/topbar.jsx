9a5456b6d680d4a6c8f5708196843b6c8a8f650d
Jeanhuitre <91871206+Jeanhuitre@users.noreply.github.com>
2022-01-28 16:29:09 +0100

    modif topbar

--- src/components/Topbar/topbar.jsx
+++ src/components/Topbar/topbar.jsx
@@ -1,24 +1,33 @@
 import "./topbar.scss";
-import {} from "@material-ui/icons";
+import {Person, Mail} from "@material-ui/icons";
 
 export default function topbar() {
   return (
+    <div className="topbar">
+        <div className="wrapper">
+            <div className="left">
+                <a href="#intro" className="logo"> Moi. </a>
+                <div className="itemContainer">
+                    <Person className="icon" />
+                    <span>+33 6 34 56 46 62</span>
+                </div>
+
+                <div className="itemContainer">
+                    <Mail className="icon" />
+                        <span>lucas.merino@outlook.fr</span>
+                </div>
+            </div>
+
+            <div className="right">
+              <div className="hamburger">
+                  <span className="line1"></span>
+                  <span className="line2"></span>
+                  <span className="line3"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+ )
+
   
-  <div className="topbar">
-      <div className="wrapper">
-          <div className="left">
-            <a href="#intro" className="logo"> Moi. </a>
-          </div>
-
-          <div className="itemContainer">
-
-          </div>
-
-          <div className="right">
-              C'est à droite
-          </div>
-      </div>
-  </div>
-
-  )
 }
